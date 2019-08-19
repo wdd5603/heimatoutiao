abc34690764a4b5d8f8c262012ce233414a1a0a8
wdd <1617022184@qq.com>
2019-08-19 20:45:09 +0800

    项目准备工作

---
+++ src/views/login/Login.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="login">
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.login{
+width: 100%
+height: 100vh
+background-image: url('../../assets/images/login_bg.jpg')
+background-size: cover
+}
+</style>
